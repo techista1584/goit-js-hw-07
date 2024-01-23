cfe9775d8c402e82d9eeb1fd0f31e6cc05759d8a
Ruby Mateo <119954182+techista1584@users.noreply.github.com>
2024-01-23 21:29:52 +0800

    updated task1& 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector("ul.gallery"); //Implementing delegation to ul.gallery
+let instance = null;
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault(); //Opening a modal window by clicking on a gallery item & Disable default behavior.
+  if (event.target.nodeName !== "IMG") return;
+  const largeImageUrl = event.target.dataset.source;
+  instance = basicLightbox.create(
+    `
+        <img src="${largeImageUrl}" width="800" height="600">
+    `
+  );
+  instance.show();
+});
+
+//Add modal window closing upon pressing the Escape key
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape" && instance) {
+    instance.close();
+    instance = null;
+  }
+});
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+let lightbox = new SimpleLightbox(".gallery__item a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
+
 console.log(galleryItems);
